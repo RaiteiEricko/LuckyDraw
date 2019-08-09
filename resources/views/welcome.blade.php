44d8bf8602c2b5287245a89ebfaa9a8bbab87622
vagrant <vagrant@homestead>
2019-08-09 13:04:40 +0000

    feat: configure basic auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Lucky Draw
                 </div>
 
                 <div class="links">
